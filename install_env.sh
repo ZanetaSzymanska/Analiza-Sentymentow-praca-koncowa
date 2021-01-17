044926d5ec44ba563cb1a5f733e3e3b87f0c1313
ZanetaSzymanska <58697811+ZanetaSzymanska@users.noreply.github.com>
2021-01-17 14:23:23 +0100

    Update install_env.sh

--- install_env.sh
+++ install_env.sh
@@ -5,12 +5,12 @@ sudo apt remove python-openssl
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py --force-reinstall
 
-python3 -m pip install scikit-learn
-python3 -m pip install -U pip setuptools wheel
-python3 -m pip install -U spacy
-python3 -m spacy download en
-python3 -m spacy download en_core_web_sm
-python3 -m spacy download xx_ent_wiki_sm
+#python3 -m pip install scikit-learn
+#python3 -m pip install -U pip setuptools wheel
+#python3 -m pip install -U spacy
+#python3 -m spacy download en
+#python3 -m spacy download en_core_web_sm
+#python3 -m spacy download xx_ent_wiki_sm
 
 python3 -m pip install virtualenv
 mkdir gettweets
@@ -20,9 +20,9 @@ source gettweets/bin/activate
 pip install tweepy progressbar pyOpenSSL requests[security]
 pip install TextBlob
 
-pip install scikit-learn
-pip install -U pip setuptools wheel
-pip install -U spacy
-python -m spacy download en
-python -m spacy download en_core_web_sm
-python -m spacy download xx_ent_wiki_sm
+#pip install scikit-learn
+#pip install -U pip setuptools wheel
+#pip install -U spacy
+#python -m spacy download en
+#python -m spacy download en_core_web_sm
+#python -m spacy download xx_ent_wiki_sm
